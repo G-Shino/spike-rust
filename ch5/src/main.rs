30cfd255ed337d3cd3c31c40a14097cbfb7ef5a3
G-shino <shinogu2929@gmail.com>
2020-06-16 01:18:43 +0900

    add ch5

--- ch5/src/main.rs
+++ ch5/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
 }
+
+impl Rectangle{
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle {width: size, height: size}
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle{width: 30, height: 50};
+    let rect2 = Rectangle{width: 10, height: 40};
+    let rect3 = Rectangle{width: 60, height: 45};
+    let sq = Rectangle::square(3);
+    println!("rect: {:#?}", rect1);
+    println!("rect: {:#?}", sq);
+    println!("The area: {}", rect1.area());
+    println!("Can rect1 hold rect2 ? : {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3 ? : {}", rect1.can_hold(&rect3));
+}
+
+// fn area(rectangle: Rectangle) -> u32{
+//     rectangle.height * rectangle.width
+// }
