fc6a1f5a634e7e7cab07e2d7b2c2179bf8fc9afb
G-shino <shinogu2929@gmail.com>
2020-06-18 13:06:44 +0900

    add try mod namespace

--- ch7-2/src/lib.rs
+++ ch7-2/src/lib.rs
@@ -3,9 +3,31 @@ mod outermost {
         middle_secret_function()
     }
     fn middle_secret_function(){}
-    pub mod inside{
+    mod inside{
         pub fn inner_function(){}
         fn secret_function(){}
+        fn secret_function_second(){
+            self::secret_function();
+        }
+    }
+    mod inside_second{
+        pub fn inner_function(){}
+    }
+    pub fn middle_function_second(){
+        self::inside_second::inner_function();
+    }
+    mod inside_third{
+        pub fn inner_function(){
+            super::inside_second::inner_function();
+            super::inside::inner_function();
+            crate::outermost::inside::inner_function();
+        }
+    }
+    mod inside_use{
+        use crate::outermost::inside_third;
+        pub fn inner_function(){
+            inside_third::inner_function();
+        }
     }
 }
 
@@ -41,7 +63,7 @@ fn main(){
 fn try_me(){
     outermost::middle_function();
     // outermost::middle_secret_function();
-    outermost::inside::inner_function();
+    // outermost::inside::inner_function();
     // outermost::inside::secret_function();
 }
 
